304a55db55e4463613c0533cfb4198b046c5811d
shagun1021 <shagunthakur89239@gmail.com>
2024-10-08 17:00:49 +0530

    fixed routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,90 +1,85 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Index from './Index'
-import './index.css'
-import Login from './Login'
-import Signup from './Signup'
-import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Index from "./Index";
+import "./index.css";
+import Login from "./Login";
+import Signup from "./Signup";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import Dashboard2 from './Dashboard2'
-import Question from './Question'
-import Quiz from './Quiz'
-import Profile from './Profile'
-import EditProfile from './EditProfile'
-import AllUser from './AllUser'
-import Like from './Like'
-import Reminder from './Reminder'
-import Nav from './Nav'
+import Dashboard2 from "./Dashboard2";
+import Question from "./Question";
+import Quiz from "./Quiz";
+import Profile from "./Profile";
+import EditProfile from "./EditProfile";
+import AllUser from "./AllUser";
+import Like from "./Like";
+import Reminder from "./Reminder";
+import Nav from "./Nav";
 
-const router=createBrowserRouter(
-    [
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Index />,
+  },
+  {
+    path: "/Nav",
+    element: <Nav />,
+    children: [
       {
-        path :"/",
-        element :<Index/>
+        path: "",
+        element: <Dashboard2 />,
       },
       {
-        path :"/Nav",
-        element :<Nav/>,
-        children :[
-          {
-            
-              path:"",
-              element:<Dashboard2/>
-            },
-            {
-              path:"Question",
-              element:<Question/>
-            },
-            {
-              path:"Quiz",
-              element:<Quiz/>
-            },
-            {
-              path:"Profile",
-              element:<Profile/>,
-             
-              
-            },
-            {
-              path:"EditProfile",
-              element:<EditProfile/>
-            },
-            
-            
-            {
-              path:"AllUser",
-              element:<AllUser/>
-            },
-            {
-              path:"Like",
-              element:<Like/>
-            },
-            {
-              path:"Reminder",
-              element:<Reminder/>
-            },
-            {
-              path:"Dashboard",
-              element:<Dashboard2/>
-            } 
-          
-        ]
+        path: "Dashboard/Question",
+        element: <Question />,
       },
       {
-        path:"/login",
-        element:<Login/>
+        path: "Quiz",
+        element: <Quiz />,
       },
       {
-        path:"/Signup",
-        element:<Signup/>
+        path: "Profile",
+        element: <Profile />,
       },
-     
-      
-    ]
-  )
-  
-ReactDOM.createRoot(document.getElementById('root')).render(
+      {
+        path: "Profile/EditProfile",
+        element: <EditProfile />,
+      },
+      {
+        path: "Dashboard/Question/EditProfile",
+        element: <EditProfile />,
+      },
+
+      {
+        path: "AllUser",
+        element: <AllUser />,
+      },
+      {
+        path: "Like",
+        element: <Like />,
+      },
+      {
+        path: "Reminder",
+        element: <Reminder />,
+      },
+      {
+        path: "Dashboard",
+        element: <Dashboard2 />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/Signup",
+    element: <Signup />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
